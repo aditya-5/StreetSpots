d479005806f7c566530eabf1fbee0e9aa221f8b6
Aditya Agarwal <aditya.7@outlook.com>
2021-03-14 17:41:48 +0530

    Added slides and carousel

--- index.php
+++ index.php
@@ -13,6 +13,36 @@
 
 <body>
 
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./assets/img/1.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./assets/img/2.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./assets/img/3.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="./assets/img/4.png" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
 
 
       <div class="mainHeader">
@@ -50,15 +80,15 @@
       p {text-align: center;}
       div {text-align: center;}
       </style>
-   
+
       <h3>Why Support Your Local Street Vendors?</h3>
       <p>Street vending enlivens urban public spaces and increases public safety by making streets vibrant and welcoming, Promoting street vending can generate employment, keep people safe and create the vitality and comity that is the hallmark of livable humane cities.
-      </p>   
-         
+      </p>
+
     <h1>   </h1>
     <h1>FOLLOW COVID-19 CDC GUIDELINES</h1>
     <h2>Wear a mask and maintain 6 FT from others!</h2>
-        
+
     <div>
 
 
