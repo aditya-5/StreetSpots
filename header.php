e796f96c1ffbfd3e9c34728904a4fbe78c88075e
Aditya Agarwal <aditya.7@outlook.com>
2021-03-14 15:17:23 +0530

    Changed and added colour coding

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['loggedin'])){
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
    <script src="https://kit.fontawesome.com/c4b713ed09.js" crossorigin="anonymous"></script>
    <script src="./assets/script.js" defer></script>
-   <link rel="stylesheet" href="./assets/style.css"/>
+   <link rel="stylesheet" href="./assets/style.css">
  </head>
 
 <nav class="navbar navbar-expand-lg ">
@@ -25,7 +25,7 @@ if(isset($_SESSION['loggedin'])){
        <li class="nav-item"><a class="nav-link" href="index">Home</a></li>
        <li class="nav-item"><a class="nav-link"  href="maps">Vendors</a></li>
        <li class="nav-item"><a class="nav-link"  href="forms.php">Sign Up Your StreetSpot</a></li>
-       
+
        <?php
          if($log){
            echo "<li class='nav-item'><a class='nav-link' href='./login/logout'>Logout ($fname"." "."$lname)</a></li>";
