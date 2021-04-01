21053193b3635d335bec97cdfb539c8ad2a9d9ac
Aditya Agarwal <aditya.7@outlook.com>
2021-04-01 15:24:58 +0530

    Designed the error page

--- error.php
+++ error.php
@@ -1,11 +1,32 @@
-<?php header( "refresh:4;url=index" );
-
-include("header.php")
-
+<?php header( "refresh:6;url=home" );
+include("header.php");
  ?>
+ <body id="errorPage">
+  <center><h2 class="errorHeading">REDIRECTING IN <span id="timer">5</span></h2></center>
+   <div class="container d-flex justify-content-center mt-0">
+     <img src="./assets/error.png" class="errorImg" alt="">
+   </div>
+   <?php include("footer.php")?>
+
+   <script>
+   $(document).ready(function() {
+  var i =5
+
+  var x = setInterval(function(){
+     as();
+     i--;
+     if(i<0){
+       clearInterval(x);
+     }
+   },1000)
 
 
-<h1>PAGE NOT FOUND. REDIRECTING</h1>
+  function as(){
+    $("#timer").text(i);
+  }
 
-<?php include("footer.php")
- ?>
+
+
+   })
+   </script>
+ </body>
--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 $log = false;
 if(isset($_SESSION['loggedin'])){
@@ -17,24 +16,22 @@ if(isset($_SESSION['loggedin'])){
    <link rel="icon" href="./assets/favicon.png">
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
    <script src="https://kit.fontawesome.com/c4b713ed09.js" crossorigin="anonymous"></script>
-   <script src="./assets/script.js" defer></script>
+   <script src="./assets/script.js" ></script>
    <link rel="stylesheet" href="./assets/style.css">
  </head>
 
 <nav class="navbar navbar-expand-lg fixed-top">
-   <a class="navbar-brand" href="index">
+   <a class="navbar-brand" href="home">
       <img src="./assets/logo1.png" class="hov" width="170" height="50" alt="" class="">
     </a>
      <ul class="navbar-nav">
-       <li class="nav-item"><a class="nav-link" href="index">Home</a></li>
+       <li class="nav-item"><a class="nav-link" href="home">Home</a></li>
        <li class="nav-item"><a class="nav-link"  href="maps">Vendors</a></li>
        <?php
        if($log){
         echo " <li class='nav-item'><a class='nav-link'  href='vendor'>Add Vendor</a></li>";
        }
         ?>
-
-
        <?php
          if($log){
            echo "<li class='nav-item'><a class='nav-link' href='./login/logout'>Logout ($fname"." "."$lname)</a></li>";
